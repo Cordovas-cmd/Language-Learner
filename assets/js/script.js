61eaf6b236c63bfb86ecc03c4fcd5c4a90e2e067
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-10-26 06:08:12 -0500

    add start screen and a app flow (todos) to the js.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+// Basic App flow
+/* 
+Start screen w/ intro and instructions
+Once button is clicked:
+Start timer
+Show Spanish word
+Show English Alternatives
+User picks an answer
+Show result (right/wrong)
+Update user score
+Show next Question (if there are no more game over.)
+*/
