a0bdda18d98c3e10a19ea0ddd940b33112d9a679
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-10-26 06:26:19 -0500

    adding js functionality to show first question and choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,36 @@ Show result (right/wrong)
 Update user score
 Show next Question (if there are no more game over.)
 */
+
+
+let question = [
+    {
+        title: "Gato",
+        choices: ["dog", "cat", "bird", "fish"],
+        correct: 1
+    }
+]
+
+var currentQuestionIndex = 0;
+
+
+function showFirstQuestion(q) {
+
+    // 1. select dom element
+    let titleEl = document.getElementById("questionTitle");
+
+    // 2. Give titleEl a value.
+    titleEl.textContent = q.title;
+
+    // select all choices
+    let choices = document.querySelectorAll(".choices");
+
+    choices.forEach(function(element, index){
+        element.textContent = q.choices[index];
+    });
+
+
+}
+
+
+showFirstQuestion(question[0])
